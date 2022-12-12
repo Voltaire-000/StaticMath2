fb3cbec1c1c7bfc7f4c49563729f454dbcbef814
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-12 14:47:35 +0700

    add source cpp

---
+++ MathLibrary/MathLibrary.cpp
@@ -0,0 +1,21 @@
+#include "MathLibrary.h"
+
+double MathLibrary::Arithmetic::Add(double a, double b)
+{
+    return a+b;
+}
+
+double MathLibrary::Arithmetic::Subtract(double a, double b)
+{
+    return a-b;
+}
+
+double MathLibrary::Arithmetic::Multiply(double a, double b)
+{
+    return a*b;
+}
+
+double MathLibrary::Arithmetic::Divide(double a, double b)
+{
+    return a/b;
+}
