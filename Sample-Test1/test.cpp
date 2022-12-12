a47299d58f0787412293dd096b81b8e5ce9a581e
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-12 15:15:56 +0700

    all passing

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -8,11 +8,12 @@ TEST(TestAddition, Add) {
 
 	double resultAdd = MathLibrary::Arithmetic::Add(a, b);
 
-  EXPECT_EQ(resultAdd, 106.6);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(resultAdd, 106.6);
+	EXPECT_TRUE(true);
 }
 
 TEST(TestSubtraction, Subtraction) {
+
 	double a = 7.6;
 	double b = 99;
 	
@@ -30,5 +31,16 @@ TEST(TestMultiplication, Multiply) {
 	double resultMultiply = MathLibrary::Arithmetic::Multiply(a, b);
 
 	EXPECT_EQ(resultMultiply, 247.5);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestDivision, Divide) {
+
+	double a = 2.5;
+	double b = 99;
+
+	double resultDivide = MathLibrary::Arithmetic::Divide(b, a);
+
+	EXPECT_EQ(resultDivide, 39.6);
 	EXPECT_TRUE(true);
 }
